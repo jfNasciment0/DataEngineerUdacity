616f5a34c2084b32db01ac33871b21bdd30110b5
jfnascimento <jefferson.francoo@gmail.com>
2020-03-10 23:34:29 +0000

    fix insert and load data

--- DataModeling_Redshift/sql_queries.py
+++ DataModeling_Redshift/sql_queries.py
@@ -15,6 +15,7 @@ ARN = config.get("IAM_ROLE", "ARN")
 LOG_DATA = config.get("S3", "LOG_DATA")
 LOG_JSONPATH = config.get("S3", "LOG_JSONPATH")
 SONG_DATA = config.get("S3", "SONG_DATA")
+SONG_JSONPATH = config.get("S3", "SONG_JSONPATH")
 
 # CREATE SCHEMA
 
@@ -23,18 +24,27 @@ db_schema_st_music = "CREATE SCHEMA IF NOT EXISTS st_music;"
 
 # DROP TABLES
 
-staging_events_table_drop = " DROP TABLE IF EXISTS dev.st_music.staging_event;"
-staging_songs_table_drop = " DROP TABLE IF EXISTS dev.st_music.staging_song;"
-songplay_table_drop = " DROP TABLE IF EXISTS dev.dw_music.songplays;"
-user_table_drop = " DROP TABLE IF EXISTS dev.dw_music.users;"
-song_table_drop = " DROP TABLE IF EXISTS dev.dw_music.songs;"
-artist_table_drop = " DROP TABLE IF EXISTS dev.dw_music.artists;"
-time_table_drop = " DROP TABLE IF EXISTS dev.dw_music.time;"
+staging_events_table_drop = " DROP TABLE IF EXISTS sparkify.st_music.staging_event;"
+staging_songs_table_drop = " DROP TABLE IF EXISTS sparkify.st_music.staging_song;"
+songplay_table_drop = " DROP TABLE IF EXISTS sparkify.dw_music.songplays;"
+user_table_drop = " DROP TABLE IF EXISTS sparkify.dw_music.users;"
+song_table_drop = " DROP TABLE IF EXISTS sparkify.dw_music.songs;"
+artist_table_drop = " DROP TABLE IF EXISTS sparkify.dw_music.artists;"
+time_table_drop = " DROP TABLE IF EXISTS sparkify.dw_music.time;"
+
+# SELECT TABLES 
+staging_events_table_select = " SELECT COUNT(*) FROM sparkify.st_music.staging_event;"
+staging_songs_table_select = " SELECT COUNT(*) FROM sparkify.st_music.staging_song;"
+songplay_table_select = " SELECT COUNT(*) FROM sparkify.dw_music.songplays;"
+user_table_select = " SELECT COUNT(*) FROM sparkify.dw_music.users;"
+song_table_select = " SELECT COUNT(*) FROM sparkify.dw_music.songs;"
+artist_table_select = " SELECT COUNT(*) FROM sparkify.dw_music.artists;"
+time_table_select = " SELECT COUNT(*) FROM sparkify.dw_music.time;"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
-    CREATE TABLE IF NOT EXISTS dev.st_music.staging_event(
+    CREATE TABLE IF NOT EXISTS sparkify.st_music.staging_event(
                                         artists TEXT,
                                         auth TEXT,
                                         first_name TEXT,
@@ -57,7 +67,7 @@ staging_events_table_create= ("""
 """)
 
 staging_songs_table_create = ("""
-    CREATE TABLE IF NOT EXISTS dev.st_music.staging_song(
+    CREATE TABLE IF NOT EXISTS sparkify.st_music.staging_song(
                                         num_songs INTEGER,
                                         artist_id TEXT , 
                                         artist_latitude TEXT, 
@@ -73,7 +83,7 @@ staging_songs_table_create = ("""
 """)
 
 songplay_table_create = ("""
-    CREATE TABLE IF NOT EXISTS dev.dw_music.songplays(
+    CREATE TABLE IF NOT EXISTS sparkify.dw_music.songplays(
                                         songplay_id  INTEGER IDENTITY(0,1) PRIMARY KEY,
                                         start_time  TEXT NOT NULL,
                                         user_id     TEXT NOT NULL,
@@ -87,7 +97,7 @@ songplay_table_create = ("""
 """)
 
 user_table_create = ("""
-    CREATE TABLE IF NOT EXISTS dev.dw_music.users(
+    CREATE TABLE IF NOT EXISTS sparkify.dw_music.users(
                                         user_id TEXT PRIMARY KEY,
                                         first_name TEXT NOT NULL,
                                         last_name TEXT NOT NULL,
@@ -98,7 +108,7 @@ user_table_create = ("""
 """)
 
 song_table_create = ("""
-    CREATE TABLE IF NOT EXISTS dev.dw_music.songs(
+    CREATE TABLE IF NOT EXISTS sparkify.dw_music.songs(
                                         song_id TEXT PRIMARY KEY,
                                         title TEXT NOT NULL,
                                         artist_id TEXT NOT NULL,
@@ -108,7 +118,7 @@ song_table_create = ("""
 """)
 
 artist_table_create = ("""
-    CREATE TABLE IF NOT EXISTS dev.dw_music.artists(
+    CREATE TABLE IF NOT EXISTS sparkify.dw_music.artists(
                                         artist_id TEXT PRIMARY KEY,
                                         name TEXT NOT NULL,
                                         location TEXT,
@@ -118,7 +128,7 @@ artist_table_create = ("""
 """)
 
 time_table_create = ("""
-    CREATE TABLE IF NOT EXISTS dev.dw_music.time(
+    CREATE TABLE IF NOT EXISTS sparkify.dw_music.time(
                                         start_time timestamptz PRIMARY KEY,
                                         hour int NOT NULL,
                                         day int NOT NULL,
@@ -131,47 +141,44 @@ time_table_create = ("""
 
 # STAGING TABLES
 
-staging_songs_copy  = (""" copy staging_song from {}
-credentials 'aws_iam_role={}'
-format as json 'auto' region 'us-west-2';
-        """).format(SONG_DATA, ARN)
+#staging_songs_copy  = (""" copy staging_song from {}
+#credentials 'aws_iam_role={}'
+#format as json 'auto' region 'us-west-2';
+#        """).format(SONG_DATA, ARN)
 
+staging_songs_copy  = (""" 
+COPY sparkify.st_music.staging_song from  '{}'
+CREDENTIALS 'aws_iam_role={}'
+REGION 'us-west-2'
+FORMAT AS json '{}' ;
+""").format(SONG_DATA, ARN, SONG_JSONPATH)
 
 staging_events_copy = ("""
-copy staging_events from '{}'
-credentials 'aws_iam_role={}'
-json '{}' compupdate on region 'us-west-2';
+COPY sparkify.st_music.staging_event from '{}'
+CREDENTIALS 'aws_iam_role={}'
+REGION 'us-west-2'
+FORMAT AS json '{}' ;
 """).format(LOG_DATA, ARN, LOG_JSONPATH)
 
 
 # FINAL TABLES
 
-songplay_table_insert = ("""
-begin transaction;
-
-    insert into dev.dw_music.songplays  (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-    select ts, user_id, level, song_id, staging_song.artist_id, session_id, location, user_agente
-    from dev.st_music.staging_event 
-    inner join dev.st_music.staging_song On staging_event.song = staging_song.title and
-                               staging_event.artists = staging_song.artist_name
-    ;
-
-end transaction;
-""")
-
 user_table_insert = ("""
 begin transaction;
 
-    delete from dev.dw_music.users 
-    using dev.st_music.staging_event 
+    delete from sparkify.dw_music.users 
+    using sparkify.st_music.staging_event 
     where   staging_event.first_name = users.first_name
             and staging_event.last_name = users.last_name
             and staging_event.gender = users.gender
             and staging_event.level = users.level
     ; 
 
-    insert into dev.dw_music.users 
-    select user_id, first_name, last_name, gender, level from dev.st_music.staging_event;
+    insert into sparkify.dw_music.users 
+    select user_id, first_name, last_name, gender, level 
+        from sparkify.st_music.staging_event
+        Where user_id is not null
+    ;
 
 end transaction;
 
@@ -180,16 +187,19 @@ end transaction;
 song_table_insert = ("""
 begin transaction;
 
-    delete from dev.dw_music.songs 
-    using dev.st_music.staging_song 
+    delete from sparkify.dw_music.songs 
+    using sparkify.st_music.staging_song 
     where   staging_song.title = songs.title
             and staging_song.artist_id = songs.artist_id
             and staging_song.year = songs.year
             and staging_song.duration = songs.duration
     ;
 
-    insert into dev.dw_music.songs 
-    select song_id , title , artist_id , year , duration from dev.st_music.staging_song;
+    insert into sparkify.dw_music.songs 
+    select song_id , title , artist_id , year , duration 
+        from sparkify.st_music.staging_song
+        Where song_id is not null
+    ;
 
 end transaction;
 
@@ -198,18 +208,19 @@ end transaction;
 artist_table_insert = ("""
 begin transaction;
 
-    delete from dev.dw_music.artists 
-    using dev.st_music.staging_song 
+    delete from sparkify.dw_music.artists 
+    using sparkify.st_music.staging_song 
     where   staging_song.artist_name = artists.name
             and  staging_song.artist_latitude = artists.latitude
             and  staging_song.artist_longitude = artists.longitude
+            and  staging_song.artist_location = artists.location
     ;
 
-    insert into dev.dw_music.artists 
-    select artist_id, artist_name, location, artist_latitude, artist_longitude 
-    from dev.st_music.staging_event 
-    inner join dev.st_music.staging_song On staging_event.song = staging_song.title and
-                               staging_event.artists = staging_song.artist_name;
+    insert into sparkify.dw_music.artists 
+    select artist_id, artist_name, artist_location, artist_latitude, artist_longitude 
+        from sparkify.st_music.staging_song
+        where artist_id is not null
+    ;
 
 end transaction;
 
@@ -218,29 +229,45 @@ end transaction;
 time_table_insert = ("""
 begin transaction;
 
-    delete from dev.dw_music.time 
-    using dev.st_music.staging_event 
+    delete from sparkify.dw_music.time 
+    using sparkify.st_music.staging_event 
     where        to_timestamp(ts::text, 'YYYYMMDDHH24MISS') = time.start_time
     ;
 
-    insert into dev.dw_music.time 
+    insert into sparkify.dw_music.time 
     select 
-          to_timestamp(ts::text, 'YYYYMMDDHH24MISS') 
-        , EXTRACT(HOUR FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
-        , EXTRACT(DAY FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
-        , EXTRACT(WEEK FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
-        , EXTRACT(MONTH FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
-        , EXTRACT(YEAR FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
-        , EXTRACT(DOW FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
-    from dev.st_music.staging_event;
+            to_timestamp(ts::text, 'YYYYMMDDHH24MISS') 
+            , EXTRACT(HOUR FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
+            , EXTRACT(DAY FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
+            , EXTRACT(WEEK FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
+            , EXTRACT(MONTH FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
+            , EXTRACT(YEAR FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
+            , EXTRACT(DOW FROM to_timestamp(ts::text, 'YYYYMMDDHH24MISS'))
+        from sparkify.st_music.staging_event
+        Where ts is not null
+    ;
 
 end transaction;
                    
 """)
 
+songplay_table_insert = ("""
+begin transaction;
+
+    insert into sparkify.dw_music.songplays  (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    select ts, user_id, level, song_id, staging_song.artist_id, session_id, location, user_agente
+        from sparkify.st_music.staging_event 
+        inner join sparkify.st_music.staging_song On staging_event.song = staging_song.title and
+                                                     staging_event.artists = staging_song.artist_name
+        WHERE staging_event.page = 'NextSong'
+    ;
+
+end transaction;
+""")
 # QUERY LISTS
 create_schema_queries = [db_schema_dw_music, db_schema_st_music]
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+insert_table_queries = [user_table_insert, song_table_insert, artist_table_insert, time_table_insert, songplay_table_insert]
+select_table_queries = [staging_events_table_select, staging_songs_table_select, songplay_table_select, user_table_select, song_table_select, artist_table_select, time_table_select]
